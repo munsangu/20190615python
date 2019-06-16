1b819d11937deff396959fde6f97b3ad099e300f
munsangu <ds-3632@hanmail.net>
2019-06-16 15:54:15 +0900

    20190616 다른 if 예제 추가

--- python_basic/if_test 1.py
+++ python_basic/if_test 1.py
@@ -1,6 +1,14 @@
+# 주석
+'''
 money = True
 if money:
     print("택시를 타고 가라")
 else:
     print("걸어 가라")
-    
+''' 
+
+pocket = ['paper','cellphone']
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
