bf421880c3dcd9488844adacf20ac8b9cc2c9b92
munsangu <ds-3632@hanmail.net>
2019-07-07 21:00:40 +0900

    190707 학습

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,50 @@
-from flask import Flask 
+from flask import Flask, render_template,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes!'
+
+@app.route('/user/<username>')
+def user(username):
+    return "User %s" % username
+
+@app.route('/user/<username>/<int:age>')
+def user1(username,age):
+    # return "Username %s, age %d"%(username,age)
+    return render_template('index.html', username=username, age=age)
+
+@app.route('/forminput/')
+def forminput():
+    return render_template('form_input.html')
+
+@app.route('/method/',methods=['GET','POST'])
+def method():
+    if request.method == "POST":
+        return "POST"
+    else:
+        return "GET"
+
+@app.route('/login/')
+def login():
+    username = request.args.get('name')
+    return  render_template('index.html',username=username)       
+
+@app.route('/form_input1/')
+def forminput1():
+    return render_template('form_input1.html')
+
+@app.route('/login/', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    return  render_template('index.html',username=username, password=password)  
+
+
 if __name__=='__main__':
     app.run(debug=True)
---
+++ flask/app2.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form_input2.html')
+
+@app.route('/login/',methods=['POST'])
+def login():
+        result=request.form.to_dict()
+        print(type(result))
+        return render_template('form_result.html',result=result)    
+
+if __name__=='__main__':
+    app.run(debug=True)
